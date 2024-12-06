fb7780a42af5f24579cb01a720fdb179d4d5e9f1
Vinicius32 <viniciuscsw1.0@gmail.com>
2024-12-06 10:41:52 -0300

    projeto de perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual é a função usada em Python para exibir algo na tela?',
+    'print()'
+)
+
+criaCartao(
+    'História',
+    'Em que ano começou a Primeira Guerra Mundial?',
+    '1914'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a estrutura de controle em Python usada para executar um bloco de código repetidamente enquanto uma condição for verdadeira?',
+    'while'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é o nome do autor de "Dom Casmurro"?',
+    'Machado de Assis'
+)
